dce7444e259cbc72235bcd5528408070d18cdfd7
marcinooo <mawachacki@gmail.com>
2019-10-23 22:37:29 +0200

    First working version

---
+++ src/philips_air_purifier/__init__.py
@@ -0,0 +1,10 @@
+from ._air_purifier import PhilipsAirPurifier
+from ._utils import ALLOWED_PARAMETERS_TO_SET
+from . import errors
+
+
+__all__ = [
+    'PhilipsAirPurifier',
+    'ALLOWED_PARAMETERS_TO_SET',
+    'errors',
+]
